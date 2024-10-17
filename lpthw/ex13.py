dffd520d1054dabd4ad8fc3d852e9ff164173b18
Job King <jobezking@gmail.com>
2024-10-17 17:09:56 -0400

    ex12.py ex13.py ex14.py ex15.py

---
+++ lpthw/ex13.py
@@ -0,0 +1,9 @@
+from sys import argv
+# read the WYSS section for how to run this
+#python3 ex13.py first 2nd 3rd
+script, first, second, third = argv
+
+print("The first script is called:", script)
+print("The first variable is:", first)
+print("The second variable is:", second)
+print("Your third variable is:", third)
