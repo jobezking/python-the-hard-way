8f7553d6a84b40649ce921e72eef191beef37d51
Job King <jobezking@gmail.com>
2024-10-17 16:44:39 -0400

    ex10.py ex11.py

--- lpthw/ex10.py
+++ lpthw/ex10.py
@@ -1,7 +1,15 @@
-print("How old are you?", end=' ')
-age = input()
-print("How tall are you?", end=' ')
-height = input()
-print("How much do you weigh?", end=' ')
-weight = input()
-print(f"So you are {age} old, {height} tall and {weight} heavy")
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
