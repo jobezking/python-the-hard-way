3f0fef235099a3238237a73aedc2939c78ca4472
Job King <jobezking@gmail.com>
2024-10-17 21:43:43 -0400

    ex24.py

---
+++ lpthw/ex23.py
@@ -0,0 +1,22 @@
+import sys
+script, encoding, error = sys.argv
+
+
+def main(language_file, encoding, errors):
+    line = language_file.readline()
+
+    if line:
+        print_line(line, encoding, errors)
+
+def print_line(line, encoding, errors):
+    next_lang = line.strip()
+    raw_bytes = next_lang.encode(encoding, errors=errors)
+    cooked_string = raw_bytes.decode(encoding, errors=errors)
+
+    print(raw_bytes, "<===>")
+
+
+languages = open("languages.txt", encoding="utf-8")
+
+main(languages, encoding, error)
+#python3 ex23.py big5 strict
