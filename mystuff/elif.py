e4280725f6272dbcf67e69ee31a8a9291f85d6b1
Job King <jobezking@gmail.com>
2025-02-11 18:05:19 -0500

    elif.py

---
+++ mystuff/elif.py
@@ -0,0 +1,10 @@
+points = 300
+
+if points >= 1000:
+  print("You've achieved Platinum status! Enjoy exclusive benefits.")
+elif points >= 500:
+  print("You're a Gold member! Thank you for your loyalty.")
+elif points >= 100:
+  print("Welcome to the Silver tier! Start earning rewards.")
+else:
+  print("You're a Bronze member. Keep shopping to earn more points!")
