9363391ff202d4a649a44ade71fbccf9ae1e301c
Job King <jobezking@gmail.com>
2025-02-12 16:07:11 -0500

    area_of_rectangle function

--- datascience/functions/function.py
+++ datascience/functions/function.py
@@ -2,4 +2,8 @@ def greet(name):
   """This function greets the user by name."""
   print(f"Hello, {name}!")
 
+def area_of_rectangle(height, width):
+  """This function calculates the area of a rectangle."""
+  return height * width
+
 greet("Alice")
