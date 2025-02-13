750ccdde7d8948dcaf5e19d524fa0465e6ecbe72
Job King <jobezking@gmail.com>
2025-02-13 15:09:24 -0500

    jj

--- datascience/dictionaries/modify_dicts_lists.py
+++ datascience/dictionaries/modify_dicts_lists.py
@@ -15,3 +15,13 @@ del item_quantities["apples"]
 # Print updated list and dictionary
 print(shopping_list)
 print(item_quantities)
+
+product_catalog = {
+    "SKU123":{"name":"Widget A","price":19.99,"quantity":50},
+    "SKU456":{"name":"Gadget B","price":34.95,"quantity":25},
+    "SKU789":{"name":"Gizmo C","price":9.99,"quantity":100}
+}
+
+sku = product_catalog.get("SKU123")
+price = sku.get("price")
+print(price)
