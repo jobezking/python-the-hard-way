f59099c79c810a62a45f0e2c783d740deae2b2cf
Job King <jobezking@gmail.com>
2025-04-20 22:18:53 -0400

    myfirstml.py

---
+++ mystuff/myfirstml.py
@@ -0,0 +1,8 @@
+#Import the required Python library and load dataset.
+import pandas as pd
+df = pd.read_csv("/home/user/Downloads/data/small_csv_files/marine-non-indigenous-species-m-codes-dictionary.csv")
+
+#Display the First 5 Rows
+df.head()
+
+#So, requires Jupyter
